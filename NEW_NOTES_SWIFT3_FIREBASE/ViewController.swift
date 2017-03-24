948a258eca70797e042b6c249706536a39e0f959
01GOD <GOD@gods-bot.local>
2017-03-24 22:10:42 +0800

    Initial Commit

---
+++ NEW_NOTES_SWIFT3_FIREBASE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NEW_NOTES_SWIFT3_FIREBASE
+//
+//  Created by GOD on 3/24/17.
+//  Copyright © 2017 ALL ONE SUN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
